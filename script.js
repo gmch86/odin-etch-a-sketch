0dcf396ada28c1b6a95fcf06c79f30e5ef3f4ebf
Grant <76251920+gmch86@users.noreply.github.com>
2026-09-06 02:39:38 +0100

    feat(script.js): fillTile function
    - Accepts row, column, and color as parameters
    - Fills the specified tile with the given color

--- script.js
+++ script.js
@@ -3,20 +3,25 @@ function createGrid(size = 16) {
     const rowElement = document.createElement("div");
 
     rowElement.classList.add("row");
-    rowElement.dataset.row = row + 1;
+    rowElement.dataset.row = row;
 
     for (let col = 0; col < size; col++) {
       const tileElement = document.createElement("div");
 
       tileElement.classList.add("tile");
-      tileElement.dataset.row = row + 1;
-      tileElement.dataset.col = col + 1;
+      tileElement.dataset.row = row;
+      tileElement.dataset.col = col;
 
       rowElement.append(tileElement);
     }
 
     document.querySelector(".canvas-container").append(rowElement);
   }
+}
+
+function fillTile(row, col, color = "") {
+  const tile = document.querySelector(`[data-row='${row}'][data-col='${col}']`);
+  tile.style.backgroundColor = color;
 }
 
 createGrid();
